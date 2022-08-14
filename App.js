8a583364cb8c7b9cf6284b0c5f0968a277b0ce14
Vladimir Dzyuba <vovadzyuba14@gmail.com>
2022-08-14 22:06:16 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,24 @@
 import React, {Component} from "react"
+import {Posts} from './components/Posts'
 
 class App extends Component {
     state = {
-        count: +localStorage.getItem('count') || 0,
-        start: true,
+        posts: [
+            {id: 'abc1', name: 'JS Basics'},
+            {id: 'abc2', name: 'JS Advanced'},
+            {id: 'abc3', name: 'React JS'},
+        ]
     };
 
-    startStop = () => {
-        this.state.start ?  this.setState({start: false}) : this.setState({start: true})
+    deleteStr = () => {}
 
-        this.state.start ?
-            (this.timerId = setInterval(() => {
-            this.setState({count: this.state.count + 1});}, 1000)) :
-            clearInterval(this.timerId)
-    }
-
-    reset = () => {
-        this.setState({count: 0, start: true})
-        clearInterval(this.timerId)
-    }
-
-    componentDidUpdate() {
-        localStorage.setItem('count', this.state.count)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerId)
-    }
 
     render() {
+        const {posts} = this.state;
+
         return (
-            <div className="App" style={{margin: 'auto', width: '300px', textAlign: "center"}}>
-                <h1>React Timer</h1>
-                <h3>{this.state.count}</h3>
-                <button onClick={this.startStop}>{this.state.start ? 'Start' : 'Stop'}</button>
-                <button onClick={this.reset}>Reset</button>
+            <div className='App'>
+                <Posts posts={posts}/>
             </div>
         );
     }
---
+++ components/Post.jsx
@@ -0,0 +1,5 @@
+export function Post (props) {
+    const {name} = props;
+
+    return <h2>{name} <button>delete</button></h2>
+}
